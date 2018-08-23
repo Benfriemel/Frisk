061800510de5fb6a2c29c66b5e9bca24efaed790
Benjamin Ryan Friemel <bfriemel@cse.unl.edu>
2018-08-23 15:06:03 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ben Friemel Petr Stadnik
+ * Date: 8/21/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ben Friemel Undeclared\n");
+
+  return 0;
+}
